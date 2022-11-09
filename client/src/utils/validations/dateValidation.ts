8a3f149d125e2b912da69efa01fd58140ac36a4d
Erik Lopes <erik.lopesoliveira2019@gmail.com>
2022-11-09 14:15:30 -0300

    chore(dateValidation): adicionando ano maximo na verificação)

--- client/src/utils/validations/dateValidation.ts
+++ client/src/utils/validations/dateValidation.ts
@@ -7,7 +7,7 @@ const validateDate = (date: string | null): boolean => {
   const month = dateSplit[1];
   const day = dateSplit[1];
   const re = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
-  return re.test(date) && year > '1900' && month > '00' && month < '13' && day > '00' && day < '32';
+  return re.test(date) && year > '1900' && month > '00' && month < '13' && day > '00' && day < '32' && year < '2022';
 };
 
 export default validateDate;
